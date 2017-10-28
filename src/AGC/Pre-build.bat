35a1d5c712ae7f954fa9425aa72357fcc0c9f985
BogDan Vatra <bogdan@kdab.com>
2017-10-28 21:04:02 +0300

    Remove non client files.

--- src/AGC/Pre-build.bat
+++
@@ -1,5 +0,0 @@
-cscript /e:jscript /NoLogo ..\src\AGC\XMLXForm.js ..\src\AGC\AGCEvents.xml ..\src\AGC\AGCEventsCPP.xsl %1AGCEventsCPP.h
-cscript /e:jscript /NoLogo ..\src\AGC\XMLXForm.js ..\src\AGC\AGCEvents.xml ..\src\AGC\AGCEventsRC2.xsl %1AGCEventsRC2.rc2
-cscript /e:jscript /NoLogo ..\src\AGC\XMLXForm.js ..\src\AGC\AGCEvents.xml ..\src\AGC\AGCEventsRCH.xsl %1AGCEventsRCH.h
-cscript /e:jscript /NoLogo ..\src\AGC\XMLXForm.js ..\src\AGC\AGCEvents.xml ..\src\AGC\AGCEventsMC.xsl %1AGCEventsMC.mc
-mc.exe -r %1 -h %1 %1AGCEventsMC.mc
--- src/Inc/allegdb.h
+++ src/Inc/allegdb.h
@@ -14,7 +14,7 @@
 #include <oledb.h>
 #include <sqloledb.h>
 #include <atldbcli.h>
-#include <..\test\TCLib\TCThread.h>
+//#include <..\test\TCLib\TCThread.h>
 
 class CSQLCore;
 class CSQLQuery;
